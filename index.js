4e32ba0ad54cc09c2ffa1b16168d1d2d2b99df2c
Nathan Kluth <nlkluth@gmail.com>
2014-03-12 16:17:13 -0600

    init the db

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var debug = require('debug')('shim:server');
 var app = express();
+var db  = require('./database');
 
 var port = process.env.PORT || 3000;
+db.init();
 
 /** Configuration **/
 
